bce5cab35faf0d40c3cccca40a2baded7ae052f6
Ronni Chidekel <ronnidc@gmail.com>
2017-02-27 14:23:49 +0100

    chapter: Quick Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+// [ ] means property binding - Component to DOM
+// ( ) means event binding - DOM to Component
+
+  title = 'Customer App';
+  name = 'Ward';
+  wardsColor = 'darkgreen';
+  customers = [
+    {id: 1, name: 'Ward'},
+    {id: 2, name: 'Kevin'},
+    {id: 3, name: 'Eric'},
+    {id: 4, name: 'Sally'},
+    {id: 5, name: 'Emmet'}
+  ];
+
+  changeSuitColor() {
+    // this.wardsColor = 'red';
+    this.wardsColor = this.wardsColor === 'darkgreen' ?
+    'darkred' : 'darkgreen';
+  }
+}
